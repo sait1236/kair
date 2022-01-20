33025ba5842825893fba51e62f4acb52461d6a14
sait1236 <97965519+sait1236@users.noreply.github.com>
2022-01-20 21:55:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let page = document.getElementById("aq");
+let white = document.getElementById("ak");
+page.addEventListener("click",()=>{
+  document.body.style.backgroundColor="grey";
+})
+white.addEventListener("click",()=>{
+    document.body.style.backgroundColor="white";
+  })
+
+
